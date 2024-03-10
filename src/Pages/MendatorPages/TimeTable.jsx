807ed1de423935867a3edcaf2c197c51239e4773
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-03-10 23:03:59 +0530

    Add the time table

--- src/Pages/MendatorPages/TimeTable.jsx
+++ src/Pages/MendatorPages/TimeTable.jsx
@@ -1,119 +1,96 @@
 import React from 'react'
+import NavigateArrow from '../../Component/NavigateArrow'
+import Footer from '../../Component/Footer'
 
-const TimeTable = () => {
-    
-    const theam = useSelector((state) => { return state.Theam })
+function TimeTable() {
     return (
-        <div class='flex flex-col'>
-            <div class='overflow-x-auto sm:mx-0.5 lg:mx-0.5'>
-                <div class='py-2 inline-block min-w-full sm:px-6 lg:px-8'>
-                    <div class='overflow-hidden'>
-                        <table class='min-w-full'>
-                            <thead class='bg-white border-b'>
-                                <tr>
-                                    <th
-                                        scope='col'
-                                        class='text-sm font-medium text-gray-900 px-6 py-4 text-left'
-                                    >
-                                        #
-                                    </th>
-                                    <th
-                                        scope='col'
-                                        class='text-sm font-medium text-gray-900 px-6 py-4 text-left'
-                                    >
-                                        First
-                                    </th>
-                                    <th
-                                        scope='col'
-                                        class='text-sm font-medium text-gray-900 px-6 py-4 text-left'
-                                    >
-                                        Last
-                                    </th>
-                                    <th
-                                        scope='col'
-                                        class='text-sm font-medium text-gray-900 px-6 py-4 text-left'
-                                    >
-                                        Handle
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class='bg-gray-100 border-b'>
-                                    <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
-                                        1
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Mark
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Otto
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        @mdo
-                                    </td>
-                                </tr>
-                                <tr class='bg-white border-b'>
-                                    <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
-                                        2
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Jacob
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Dillan
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        @fat
-                                    </td>
-                                </tr>
-                                <tr class='bg-gray-100 border-b'>
-                                    <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
-                                        3
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Mark
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Twen
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        @twitter
-                                    </td>
-                                </tr>
-                                <tr class='bg-white border-b'>
-                                    <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
-                                        4
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Bob
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        Dillan
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        @fat
-                                    </td>
-                                </tr>
-                                <tr class='bg-gray-100 border-b'>
-                                    <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
-                                        5
-                                    </td>
-                                    <td
-                                        colspan='2'
-                                        class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center'
-                                    >
-                                        Larry the Bird
-                                    </td>
-                                    <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                                        @twitter
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+        <>
+            <NavigateArrow />
+            <div className=' h-screen flex items-center justify-center flex-col'>
+                <h1 className=' text-3xl font-bold capitalize font-serif mb-7'>College of management and computer science, yavatmal</h1>
+                <p className=' capitalize font-semibold mb-8  text-2xl  '>bachelor of computer application part-3</p>
+                <div className=' w-[80vw] h-[60vh] mb-20'>
+                    <table className='w-full h-full'>
+                        <thead>
+                            <tr>
+                                <th className='border-2 border-black px-2 py-5' >Days</th>
+                                    <th className='border-2 uppercase border-black px-2 py-5' >11.00-12.25</th>
+                                <th className='border-2  border-black px-2 py-5' >12:30-1:15</th>
+                                <th className='border-2 border-black py-5' >1:15-2:00</th>
+                                <th className='border-2 border-black px-2 py-5' >2:00-2:45</th>
+                                <th className='border-2 border-black px-2 py-5' >2:45-3:15</th>
+                                <th className='border-2 border-black px-2 py-5' >3:15-4:00</th>
+                                <th className='border-2 border-black px-2 py-5' >4.00-4:45</th>
+                                <th className='border-2 border-black px-2 py-5' >4:45-5:30</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th className='border-2 uppercase border-black px-2 py-5' >Monday</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >mmt</th>
+                                <th className='border-2 uppercase border-black py-5' colSpan={2}> lab 2</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' rowSpan={6} >RECESS</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >.net</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >st</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >adbms</th>
+                            </tr>
+                            <tr>
+                                <th className='border-2 uppercase border-black px-2 py-5' >thuesday</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >mmt</th>
+                                <th className='border-2 uppercase border-black py-5' colSpan={2}> lab 2</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >.net</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >st</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >adbms</th>
+                            </tr>
+                            <tr>
+                                <th className='border-2 uppercase border-black px-2 py-5' >wednesday</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >.net</th>
+                                <th className='border-2 uppercase border-black py-5'> cst</th>
+                                <th className='border-2 uppercase border-black py-5'> adbms</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' colSpan={2} >lab 1</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >mmt</th>
+                            </tr>
+
+                            <tr>
+                                <th className='border-2 uppercase border-black px-2 py-5' >thursday</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >.net</th>
+                                <th className='border-2 uppercase border-black py-5'> cst</th>
+                                <th className='border-2 uppercase border-black py-5'>st</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' colSpan={2} >lab 1</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                            </tr>
+
+                            <tr>
+                                <th className='border-2 uppercase border-black px-2 py-5' >friday</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >lab 3(project)</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >cst</th>
+                                <th className='border-2 uppercase border-black py-5'>.net</th>
+                                <th className='border-2 uppercase border-black py-5'>adbms</th>
+                                <th className='border-2 uppercase border-black px-2 py-5'>mmt</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >st</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                            </tr>
+
+                            <tr>
+                                <th className='border-2 uppercase border-black px-2 py-5' >saturday</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >lab 3(project)</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >cst</th>
+                                <th className='border-2 uppercase border-black py-5'>.net</th>
+                                <th className='border-2 uppercase border-black py-5'>adbms</th>
+                                <th className='border-2 uppercase border-black px-2 py-5'>mmt</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >st</th>
+                                <th className='border-2 uppercase border-black px-2 py-5' >--</th>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
